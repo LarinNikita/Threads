e2da21cf132c9ca0301ad40221fdcddcff74c305
LarinNikita <larin_nikita_m@mail.ru>
2023-12-07 23:01:09 +0700

    подключение к бд & схема пользователя

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -20,6 +20,8 @@ import { UserValidation } from "@/lib/validations/user";
 import Image from "next/image";
 import { isBase64Image } from "@/lib/utils";
 import { useUploadThing } from "@/lib/uploadthing";
+import { updateUser } from "@/lib/actions/user.actions";
+import { usePathname, useRouter } from "next/navigation";
 
 interface Props {
     user: {
@@ -38,7 +40,10 @@ const AccountProfile = ({
     btnTitle
 }: Props) => {
     const [files, setFiles] = useState<File[]>([]);
-    const { startUpload } = useUploadThing("media")
+    const { startUpload } = useUploadThing("media");
+
+    const router = useRouter();
+    const pathname = usePathname();
 
     const form = useForm({
         resolver: zodResolver(UserValidation),
@@ -85,7 +90,20 @@ const AccountProfile = ({
             }
         }
 
-        //TODO: обновить данные пользователя
+        await updateUser({
+            userId: user.id,
+            username: values.username,
+            name: values.name,
+            bio: values.bio,
+            image: values.profile_photo,
+            path: pathname
+        });
+
+        if (pathname === '/profile/edit') {
+            router.back();
+        } else {
+            router.push('/');
+        }
     };
 
     return (
