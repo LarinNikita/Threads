adf745e0be4cb369c210fcbb9ffe05374d0290e2
LarinNikita <larin_nikita_m@mail.ru>
2023-12-07 21:38:28 +0700

    установка и настройка uploadthing

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -19,6 +19,7 @@ import * as z from 'zod';
 import { UserValidation } from "@/lib/validations/user";
 import Image from "next/image";
 import { isBase64Image } from "@/lib/utils";
+import { useUploadThing } from "@/lib/uploadthing";
 
 interface Props {
     user: {
@@ -37,6 +38,7 @@ const AccountProfile = ({
     btnTitle
 }: Props) => {
     const [files, setFiles] = useState<File[]>([]);
+    const { startUpload } = useUploadThing("media")
 
     const form = useForm({
         resolver: zodResolver(UserValidation),
@@ -70,15 +72,20 @@ const AccountProfile = ({
         }
     };
 
-    function onSubmit(values: z.infer<typeof UserValidation>) {
+    const onSubmit = async (values: z.infer<typeof UserValidation>) => {
         const blob = values.profile_photo;
 
         const hasImageChange = isBase64Image(blob);
 
-        //TODO: Установить uploadthing
-        // if (hasImageChange) {
-        //     const imgRes = 
-        // }
+        if (hasImageChange) {
+            const imgRes = await startUpload(files)
+
+            if (imgRes && imgRes[0].url) {
+                values.profile_photo = imgRes[0].url;
+            }
+        }
+
+        //TODO: обновить данные пользователя
     };
 
     return (
