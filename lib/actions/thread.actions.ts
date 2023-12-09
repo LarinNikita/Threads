830715e0d0b2b4caae3ad8eb3af55813fa20fa80
LarinNikita <larin_nikita_m@mail.ru>
2023-12-09 13:42:05 +0700

    Отображение постов на главной странице. Страница с одним постом

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -36,3 +36,73 @@ export async function createThread({
         throw new Error(`Error creating thread: ${error.message}`)
     }
 }
+
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+    try {
+        connectToDB();
+
+        const skipAmount = (pageNumber - 1) * pageSize;
+
+        const postQuery = Thread.find({ parentId: { $in: [null, undefined] } })
+            .sort({ createdAt: 'desc' })
+            .skip(skipAmount)
+            .limit(pageSize)
+            .populate({ path: 'author', model: User })
+            .populate({
+                path: 'children',
+                populate: {
+                    path: 'author',
+                    model: User,
+                    select: "_id name parentId image"
+                }
+            });
+
+        const totalPostCount = await Thread.countDocuments({ parentId: { $in: [null, undefined] } });
+
+        const posts = await postQuery.exec();
+
+        const isNext = totalPostCount > skipAmount + posts.length;
+
+        return { posts, isNext }
+
+    } catch (error: any) {
+        throw new Error(`${error.message}`);
+    }
+}
+
+export async function fetchThreadById(id: string) {
+    connectToDB();
+
+    try {
+        const thread = await Thread.findById(id)
+            .populate({
+                path: 'author',
+                model: User,
+                select: "_id id name image"
+            })
+            .populate({
+                path: 'children',
+                populate: [
+                    {
+                        path: 'author',
+                        model: User,
+                        select: "_id id name parentId image"
+                    },
+                    {
+                        path: 'children',
+                        model: Thread,
+                        populate: {
+                            path: 'author',
+                            model: User,
+                            select: "_id id name parentId image"
+                        }
+                    }
+                ]
+            }).exec();
+
+        return thread;
+
+    } catch (error: any) {
+        throw new Error(`Error fetching thread: ${error.message}`);
+    }
+}
